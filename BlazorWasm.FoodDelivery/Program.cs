de6dada21bc81d174e0f02136366d1e0943105d0
Mg Chit <chitlunh@gmail.com>
2023-06-07 01:36:29 +0630

    -

--- BlazorWasm.FoodDelivery/Program.cs
+++ BlazorWasm.FoodDelivery/Program.cs
@@ -9,4 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<FoodService>();
+builder.Services.AddSingleton<NotificationStateContainer>();
 await builder.Build().RunAsync();
---
+++ BlazorWasm.FoodDelivery/Services/NotificationStateContainer.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorWasm.FoodDelivery.Services
+{
+    public class NotificationStateContainer
+    {
+        private string? savedString;
+
+        public string Property
+        {
+            get => savedString ?? "0";
+            set
+            {
+                savedString = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
